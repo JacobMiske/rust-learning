b2616847033589057d7bb11b5d06f488f764208d
Jacob Miske <jacobmiske@gmail.com>
2019-10-13 20:48:15 -0400

    variables build

---
+++ variables/target/debug/deps/variables-d7c2c5374fdcbc65.d
@@ -0,0 +1,5 @@
+/Users/jacobmiske/Documents/NOT_STW/PROJECTS/rust-learning/variables/target/debug/deps/variables-d7c2c5374fdcbc65: src/main.rs
+
+/Users/jacobmiske/Documents/NOT_STW/PROJECTS/rust-learning/variables/target/debug/deps/variables-d7c2c5374fdcbc65.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables-d7c2c5374fdcbc65.d
@@ -0,0 +1,1 @@
+/Users/jacobmiske/Documents/NOT_STW/PROJECTS/rust-learning/variables/target/debug/variables-d7c2c5374fdcbc65: /Users/jacobmiske/Documents/NOT_STW/PROJECTS/rust-learning/variables/src/main.rs
